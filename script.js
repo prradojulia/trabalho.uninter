d1d994ec902d9f67c6765454bf6c92836fcd4f15
Julia Prado Lima <166552069+prradojulia@users.noreply.github.com>
2025-06-13 16:23:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let botao = document.querySelector("#botao");
+botao.style.background="red";
+
+function mostrarMensagem(){
+    const areaMensagem = document.getElementById("mensagem");
+    areaMensagem.innerHTML = "Olá, seja bem vindo!";
+}
